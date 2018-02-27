9b4fa46a3ba2aa0c886298528fef5b97e13a3e57
Diego Muracciole <diegomuracciole@gmail.com>
2018-02-27 03:59:22 -0300

    Support min|max-height|width queries

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = function () {
+const Parser = require('./parser');
 
+module.exports = function (queries, options) {
+  let result = {};
+
+  Object.keys(queries).forEach(function(query){
+    if (Parser.parse(query).match(options)) {
+      Object.assign(result, queries[query]);
+    }
+  });
+
+  return result;
 };
