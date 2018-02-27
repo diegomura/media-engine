1f856ca6f33db74837d7d75f826e6808dfd39f92
Diego Muracciole <diegomuracciole@gmail.com>
2018-02-27 03:59:22 -0300

    Support min|max-height|width queries

---
+++ src/parser.js
@@ -0,0 +1,95 @@
+const Query = require('./queries');
+
+let NUMBERS = /[0-9]/;
+let LETTERS = /[a-z|\-]/i;
+let WHITESPACE = /\s/;
+let COLON = /:/;
+
+function tokenizer(input) {
+  let current = 0;
+  let tokens = [];
+
+  while (current < input.length) {
+    let char = input[current];
+
+    if (char === '(') {
+      tokens.push({
+        type: 'paren',
+        value: '(',
+      });
+
+      current++;
+      continue;
+    }
+
+    if (char === ')') {
+      tokens.push({
+        type: 'paren',
+        value: ')',
+      });
+      current++;
+      continue;
+    }
+
+    if (WHITESPACE.test(char) || COLON.test(char)) {
+      current++;
+      continue;
+    }
+
+    if (NUMBERS.test(char)) {
+      let value = '';
+      while (NUMBERS.test(char)) {
+        value += char;
+        char = input[++current];
+      }
+
+      tokens.push({ type: 'number', value });
+      continue;
+    }
+
+    if (LETTERS.test(char)) {
+      let value = '';
+      while (LETTERS.test(char)) {
+        value += char;
+        char = input[++current];
+      }
+
+      tokens.push({ type: 'name', value });
+      continue;
+    }
+
+    throw new TypeError('Tokenizer: I dont know what this character is: ' + char);
+  }
+
+  return tokens;
+}
+
+function parser(tokens) {
+  let current = 0;
+
+  function walk() {
+    let token = tokens[current];
+
+    if (token.type === 'number') {
+      current++;
+      return parseInt(token.value);
+    }
+
+    if (token.type === 'name') {
+      current++;
+      return Query(token.value, walk());
+    }
+
+    throw new TypeError(token.type);
+  }
+
+  return walk();
+}
+
+module.exports = {
+  parse: function(query) {
+    const tokens = tokenizer(query);
+    const ast = parser(tokens);
+    return ast;
+  }
+};
--- test/queries.test.js
+++ test/queries.test.js
@@ -1,69 +1,114 @@
 const matchMedia = require('../src');
 
-test.only('Should match max-height', () => {
+test('Should match max-height if valid', () => {
   const result = matchMedia(
-    { 'max-height: 700px': { color: 'green' }},
+    { 'max-height: 700': { color: 'green' }},
     { height: 100 },
   );
 
-  expect(result).toBe({ color: 'green' });
+  expect(result).toEqual({ color: 'green' });
 });
 
-test('Should match min-height', () => {
+test('Should not match max-height if invalid', () => {
   const result = matchMedia(
-    { 'min-height: 700px': { color: 'green' }},
+    { 'max-height: 700': { color: 'green' }},
     { height: 800 },
   );
 
-  expect(result).toBe({ color: 'green' });
+  expect(result).toEqual({});
 });
 
-test('Should match max-width', () => {
+test('Should match min-height if valid', () => {
   const result = matchMedia(
-    { 'max-width: 700px': { color: 'green' }},
+    { 'min-height: 700': { color: 'green' }},
+    { height: 800 },
+  );
+
+  expect(result).toEqual({ color: 'green' });
+});
+
+test('Should not match min-height if invalid', () => {
+  const result = matchMedia(
+    { 'min-height: 700': { color: 'green' }},
+    { height: 100 },
+  );
+
+  expect(result).toEqual({});
+});
+
+test('Should match max-width if valid', () => {
+  const result = matchMedia(
+    { 'max-width: 700': { color: 'green' }},
     { width: 100 },
   );
 
-  expect(result).toBe({ color: 'green' });
+  expect(result).toEqual({ color: 'green' });
 });
 
-test('Should match min-width', () => {
+test('Should not match max-width if invalid', () => {
   const result = matchMedia(
-    { 'min-width: 700px': { color: 'green' }},
+    { 'max-width: 700': { color: 'green' }},
     { width: 800 },
   );
 
-  expect(result).toBe({ color: 'green' });
+  expect(result).toEqual({});
 });
 
-test('Should match orientation', () => {
+test('Should match min-width if valid', () => {
+  const result = matchMedia(
+    { 'min-width: 700': { color: 'green' }},
+    { width: 800 },
+  );
+
+  expect(result).toEqual({ color: 'green' });
+});
+
+test('Should not match min-width if invalid', () => {
+  const result = matchMedia(
+    { 'min-width: 700': { color: 'green' }},
+    { width: 100 },
+  );
+
+  expect(result).toEqual({});
+});
+
+test.skip('Should match orientation if valid', () => {
   const result = matchMedia(
     { 'orientation: landscape': { color: 'green' }},
     { orientation: 'landscape' },
   );
 
-  expect(result).toBe({ color: 'green' });
+  expect(result).toEqual({ color: 'green' });
 });
 
-test('Should match and operator', () => {
+test.skip('Should not match orientation if invalid', () => {
+  const result = matchMedia(
+    { 'orientation: landscape': { color: 'green' }},
+    { orientation: 'portrait' },
+  );
+
+  expect(result).toEqual();
+});
+
+test.skip('Should match and operator', () => {
   const result = matchMedia(
     { '(max-height: 700px) and (orientation: landscape)': { color: 'green' }},
     { height: 100, orientation: 'landscape' },
   );
 
-  expect(result).toBe({ color: 'green' });
+  expect(result).toEqual({ color: 'green' });
 });
 
-test('Should match or operator', () => {
+test.skip('Should match or operator', () => {
   const result = matchMedia(
     { '(max-height: 700px), (orientation: landscape)': { color: 'green' }},
     { orientation: 'landscape' },
   );
 
-  expect(result).toBe({ color: 'green' });
+  expect(result).toEqual({ color: 'green' });
 });
 
-test('Should match several queries', () => {
+test.skip('Should match several queries', () => {
   const result = matchMedia(
     {
       'max-height: 700px': { color: 'green' },
@@ -72,5 +117,5 @@ test('Should match several queries', () => {
     { height: 100, orientation: 'landscape' },
   );
 
-  expect(result).toBe({ color: 'green', background: 'red' });
+  expect(result).toEqual({ color: 'green', background: 'red' });
 });
